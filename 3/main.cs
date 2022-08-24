ba2d166e442b9dc1ac9102a6369982e090ebecc4
Lhenrique19 <111003205+Lhenrique19@users.noreply.github.com>
2022-08-24 19:56:07 +0000

    Aula 3 - Construtores

--- 3/main.cs
+++ 3/main.cs
@@ -9,6 +9,14 @@ class Aula3 {
       "-----------------------"
     );
   }
+
+  public static void print(Disciplina disciplina){
+    Console.WriteLine(
+      "-----------------------\n" +
+      "Nome: " + disciplina.nome + "\n" + "Semestre: " + disciplina.semestre + "\n" +
+      "-----------------------"
+    );
+  }
   public static void Main (string[] args) {
 
     Aluno aluno1 = new Aluno {
@@ -16,6 +24,8 @@ class Aula3 {
       nome = "Maria",
       periodo = 0
     };
+
+    
     
     print(aluno1);
     Aluno aluno2 = new Aluno();
@@ -26,5 +36,18 @@ class Aula3 {
     print(aluno4);
     Aluno aluno5 = new Aluno("2020ENEL789", "ÉLIAGÁZINHU DELAS", 3);
     print(aluno5);
+
+    Disciplina disciplina1 = new Disciplina {
+      nome = "Cleber",
+      semestre = 0
+    };
+  
+  Disciplina disciplina2 = new Disciplina {
+      nome = "Cleberinho",
+      semestre = 11
+    };
+
+  print(disciplina1);
+  print(disciplina2);
   }
 }
--- Modelos/Disciplina.cs
+++ Modelos/Disciplina.cs
@@ -5,7 +5,20 @@ namespace Modelos {
     public string nome;
     public int semestre;
 
+    public Disciplina() : this(0){
+      
 
+    }
+    public Disciplina(int semestre){
+      if(semestre > 0){
+        if(semestre <= 10){
+        this.semestre = semestre;
+        }
+      } else {
+        this.semestre = 1;
+      }
+    
+    }
     
   }
   
